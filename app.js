0dcdc4933c71a9155b06472969f01f465e31172d
Thomas Balunis <thomfb3@gmail.com>
2022-08-14 16:06:46 -0400

    app.js and server.js setup

---
+++ app.js
@@ -0,0 +1,56 @@
+"use strict";
+/** Express app for ChoresApp. */
+const path = require('path');
+const express = require("express");
+const cors = require("cors");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const morgan = require("morgan");
+const app = express();
+
+app.use(cors());
+app.options('*', cors());
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.use(express.json());
+// Serving static files
+app.use(express.static(path.join(__dirname, 'public')));
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(cookieParser());
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+// Data sanitization against XSS
+app.use(xss());
+
+app.use(morgan("tiny"));
+
+
+
+
+
+/** Handle 404 errors -- this matches everything */
+app.use(function (req, res, next) {
+    return next(new NotFoundError());
+});
+
+/** Generic error handler; anything unhandled goes here. */
+app.use(function (err, req, res, next) {
+    if (process.env.NODE_ENV !== "test") console.error(err.stack);
+    const status = err.status || 500;
+    const message = err.message;
+
+    return res.status(status).json({
+        error: { message, status },
+    });
+});
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = require("./app");
+const { PORT } = require("./config");
+dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB, {
+  useNewUrlParser: true
+}).then(con => {
+  console.log(con.connections);
+  console.log("DB Connection successful")
+});
+
+app.listen(PORT, () => {
+  console.log(`Started on http://localhost:${PORT}`);
+});
